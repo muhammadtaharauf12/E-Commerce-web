5c73dcfc5f25fdd39d0b21695a3cb8b827009ef0
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2026-08-17 19:58:33 +0500

    Project is still ongoing

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/popular/Popular.jsx'
 import Offers from '../Components/Offers/Offers.jsx'
+import NewCollection from '../Components/NewCollection/NewCollection.jsx'
+import NewsLetter from '../Components/NewsLetter/NewsLetter.jsx'
 
 const Shop = () => {
   return (
@@ -9,6 +11,8 @@ const Shop = () => {
         <Hero/>
         <Popular/>
         <Offers/>
+        <NewCollection/>
+        <NewsLetter/>
 
     </div>
   )
